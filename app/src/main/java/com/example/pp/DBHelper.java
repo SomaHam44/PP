a01b80437bb65128ebfd9fd02ff9c81c9ddd79df
feketeand <afekete.andrea@gmail.com>
2023-12-15 11:24:16 +0100

    Adatbázis_mod

--- app/src/main/java/com/example/pp/DBHelper.java
+++ app/src/main/java/com/example/pp/DBHelper.java
@@ -15,8 +15,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String Create_Profil_Table = "CREATE TABLE IF NOT EXISTS Profil(" +
                 "profilID INTEGER NOT NULL UNIQUE," +
-                "vezeteknev TEXT," +
-                "keresztnev TEXT," +
+                "nev TEXT," +
                 "szuldatum TEXT," +
                 "TAJ INTEGER," +
                 "PRIMARY KEY(profilID AUTOINCREMENT));";
@@ -37,21 +36,3 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 }
 
-/*public class MyDatabaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "myDatabase.db";
-    public static final int DATABASE_VERSION = 1;
-
-    public MyDatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        // itt adhatjuk meg az SQL parancsokat az adatbázis tábláinak létrehozásához
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // itt adhatjuk meg az SQL parancsokat az adatbázis tábláinak frissítéséhez
-    }
-}
